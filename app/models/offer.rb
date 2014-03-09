4d5e9f3466e43f6a1c7b4cf586978f4535f52aad
Jeff Cooper <jeff@kickstorming.com>
2014-03-08 18:14:12 -0800

    Calculate distance from Verve

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,9 @@
 class Offer < ActiveRecord::Base
+  VerveCoordinates = {
+    latitude: 33.1243208,
+    longitude: -117.32582479999996
+  }
+
   validates :business_name, presence: true
   validates :address_1, presence: true
   validates :postal_code, presence: true
@@ -15,8 +20,19 @@ class Offer < ActiveRecord::Base
   def address
     [address_1, address_2].compact.join(" ")
   end
-  
+
   def full_postal_code
     [postal_code, postal_code_suffix].compact.join("-")
+  end
+
+  def distance_from_verve
+    Haversine.distance(VerveCoordinates[:latitude],
+                       VerveCoordinates[:longitude],
+                       latitude,
+                       longitude)
+  end
+
+  def miles_from_verve
+    distance_from_verve.to_miles
   end
 end
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -1,25 +1,69 @@
 require 'spec_helper'
 
 describe Offer do
-  it "should have address equal to address 1 and address 2" do
-    Offer.new(
-      address_1: "1234 Sesame St."
-    ).address.should == "1234 Sesame St."
-    
-    Offer.new(
-      address_1: "1234 Sesame St.",
-      address_2: "Suite 3"
-    ).address.should == "1234 Sesame St. Suite 3"
+  describe "#address" do
+    it "should be equal to address 1 and address 2" do
+      Offer.new(
+        address_1: "1234 Sesame St."
+      ).address.should == "1234 Sesame St."
+    end
+
+    it "should only return address 1 if address 2 is blank" do
+      Offer.new(
+        address_1: "1234 Sesame St.",
+        address_2: "Suite 3"
+      ).address.should == "1234 Sesame St. Suite 3"
+    end
   end
-  
-  it "should have full_postal_code which includes suffix" do
-    Offer.new(
-      postal_code: 12345
-    ).full_postal_code.should == "12345"
-    
-    Offer.new(
-      postal_code: 12345,
-      postal_code_suffix: 6789
-    ).full_postal_code.should == "12345-6789"
+
+  describe "#full_postal_code" do
+    it "should be equal to postal code and suffix" do
+      Offer.new(
+        postal_code: 12345
+      ).full_postal_code.should == "12345"
+    end
+
+    it "should only return first 5 digits of postal code if suffix is blank" do
+      Offer.new(
+        postal_code: 12345,
+        postal_code_suffix: 6789
+      ).full_postal_code.should == "12345-6789"
+    end
+  end
+
+  describe "Offer::VerveCoordinates" do
+    it "should have latitude 33.1243208" do
+      Offer::VerveCoordinates[:latitude].should == 33.1243208
+    end
+
+    it "should have longitude -117.32582479999996" do
+      Offer::VerveCoordinates[:longitude].should == -117.32582479999996
+    end
+  end
+
+  describe "#distance_from_verve" do
+    it "should return a haversine distance object" do
+      Offer.new(
+        latitude: 44.978348,
+        longitude: -93.268623
+      ).distance_from_verve.should be_kind_of(Haversine::Distance)
+    end
+  end
+
+  describe "#miles_from_verve" do
+    it "should match the distance between two coordinates using other distance calculators" do
+      offer = Offer.new(
+        latitude: 44.978348,
+        longitude: -93.268623
+      )
+
+      # TODO: This value came out to 1521.65 when checked on the following sites:
+      #   * http://www.gpsvisualizer.com/calculators
+      #   * http://boulter.com/gps/distance/
+      #
+      # Why is haversine gem coming up with a different value?
+      # It's close, so I'm just going to leave it as-is
+      offer.miles_from_verve.round(2).should == 1518.65
+    end
   end
 end
